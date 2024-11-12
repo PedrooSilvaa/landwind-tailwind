080a6380c871abf4490c41acaa2d5d52efc7eeaa
unknown <pedrohenriquefdasilva14@gmail.com>
2024-11-12 20:45:09 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { RouterProvider } from "react-router-dom"
-import { routes } from "./routes/routes"
 import { Header } from "./component/header"
-
 
 function App() {
 
   return (
-    <div className="w-[100%] bg-black">
-    <Header></Header>
-    <RouterProvider router={routes}></RouterProvider>
+    <div className="w-[100vw] h-[100vh]">
+        <Header></Header>
     </div>
   )
 }
--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,9 +1,29 @@
-import DarkModeToggle from "../button-darkmode";
+import logo  from '../../assets/images/landwind-logo.svg'
 
 export function Header() {
   return (
-    <div className="bg-slate-300 dark:bg-slate-800 w-100% flex">
-        <DarkModeToggle></DarkModeToggle>
+    <div className="px-10 py-5 flex flex-row w-[100%] ">
+      <div className="flex flex-row items-center gap-2 w-[12%]">
+      <img src={logo} alt="" />
+        <h1 className='text-2xl font-bold'>LandWind</h1>
+      </div>
+
+      <div className="flex fle-row items-center justify-between w-[88%]">
+        <div className="">
+          <ul className='flex flex-row gap-8 text-gray-900'>
+            <li gr>Company</li>
+            <li gr>Marketplace</li>
+            <li gr>Features</li>
+            <li gr>Team</li>
+            <li gr>Contact</li>
+          </ul>
+        </div>
+        <div className=" flex flex-row gap-4">
+            <button>Log In</button>
+            <button className='bg-[#7E3AF2] p-3 rounded-[8px] text-white font-bold'>Get started</button>
+        </div>
+      </div>
+        
     </div>
   )
 }
